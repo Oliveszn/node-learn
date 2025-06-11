e8c69da2ac4dc6cc99f8f5779b52e0f0ed9621dd
oliveszn <ekpealamicheal@gmail.com>
2025-06-11 23:14:41 +0100

    auth

---
+++ server/controller/UserController.js
@@ -0,0 +1,133 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+//register
+const registerUser = async (req, res) => {
+  console.log("REQ BODY:", req.body);
+  const { username, email, password, role } = req.body;
+  try {
+    const checkUser = await User.findOne({ $or: [{ username }, { email }] });
+    if (checkUser)
+      return res.status(400).json({
+        success: false,
+        message: "User Already exists, Please try again",
+      });
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      username,
+      email,
+      password: hash,
+      role: role || "user",
+    });
+
+    await newUser.save();
+    res.status(200).json({
+      success: true,
+      message: "successful",
+    });
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      success: false,
+      message: "Some error occured",
+    });
+  }
+};
+
+///login
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const checkUser = await User.findOne({ email });
+    if (!checkUser)
+      return res.json({
+        success: false,
+        message: "User doesn't exists! Please register first",
+      });
+
+    const checkPasswordMatch = await bcrypt.compare(
+      password,
+      checkUser.password
+    );
+    if (!checkPasswordMatch)
+      return res.json({
+        success: false,
+        message: "Incorrect password! Please try again",
+      });
+
+    const token = jwt.sign(
+      {
+        id: checkUser._id,
+        role: checkUser.role,
+        email: checkUser.email,
+        username: checkUser.username,
+      },
+      process.env.JWT_SECRET_KEY,
+      { expiresIn: "60m" }
+    );
+
+    // res.cookie("token", token, { httpOnly: true, secure: true }).json({
+    //   success: true,
+    //   message: "Logged in successfully",
+    //   user: {
+    //     email: checkUser.email,
+    //     role: checkUser.role,
+    //     id: checkUser._id,
+    //     username: checkUser.username,
+    //   },
+    // });
+    res.status(200).json({
+      success: true,
+      message: "Logged in successfully",
+      token,
+      user: {
+        email: checkUser.email,
+        role: checkUser.role,
+        id: checkUser._id,
+        username: checkUser.username,
+      },
+    });
+  } catch (e) {
+    res.status(500).json({
+      success: false,
+      message: "Some error occured",
+    });
+  }
+};
+
+///logout
+const logout = (req, res) => {
+  res.clearCookie("token").json({
+    success: true,
+    message: "Logged out succesfully",
+  });
+};
+
+const authMiddleware = async (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (!token) {
+    res.status(401).json({
+      success: false,
+      message: "Please login ",
+    });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Please login ",
+    });
+  }
+};
+
+module.exports = { registerUser, loginUser, logout, authMiddleware };
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const connectToDB = require("./database/db");
 const bookRoutes = require("./routes/BookRoutes");
+const authRoutes = require("./routes/UserRoutes");
 
 const app = express();
 app.use(express.json());
@@ -11,5 +12,6 @@ connectToDB();
 
 //routes
 app.use("/api/books", bookRoutes);
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => console.log(`server on ports ${PORT}`));
